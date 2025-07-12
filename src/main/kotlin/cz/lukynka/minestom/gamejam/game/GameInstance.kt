6e7166a63144442628c0f307426ee57d8d15b4b1
p1k0chu <anatoliyid0207@gmail.com>
2025-07-12 16:26:44 +0300

    smh useless suppression

--- src/main/kotlin/cz/lukynka/minestom/gamejam/game/GameInstance.kt
+++ src/main/kotlin/cz/lukynka/minestom/gamejam/game/GameInstance.kt
@@ -1,5 +1,3 @@
-@file:Suppress("OVERRIDE_DEPRECATION", "UnstableApiUsage", "DEPRECATION") // don't we all love adventure?
-
 package cz.lukynka.minestom.gamejam.game
 
 import cz.lukynka.minestom.gamejam.utils.PlayerListAudience
