a4397caa7441f4b30877d815872e379a02986ce9
p1k0chu <anatoliyid0207@gmail.com>
2025-07-13 22:11:13 +0300

    add util function to spawn item display

---
+++ src/main/kotlin/cz/lukynka/minestom/gamejam/utils/ItemDisplayUtils.kt
@@ -0,0 +1,18 @@
+package cz.lukynka.minestom.gamejam.utils
+
+import net.minestom.server.coordinate.Pos
+import net.minestom.server.entity.Entity
+import net.minestom.server.entity.EntityType
+import net.minestom.server.entity.metadata.display.ItemDisplayMeta
+import net.minestom.server.instance.Instance
+
+/**
+ * Returns [Entity] of type `ITEM_DISPLAY_ENTITY`
+ */
+inline fun spawnItemDisplay(world: Instance, position: Pos, block: ItemDisplayMeta.() -> Unit): Entity {
+    val entity = Entity(EntityType.ITEM_DISPLAY)
+    entity.setNoGravity(true)
+    (entity.entityMeta as ItemDisplayMeta).apply(block)
+    entity.setInstance(world, position)
+    return entity
+}
