c7e5b2106414bc97fc0969c9ed97436d902d5123
p1k0chu <anatoliyid0207@gmail.com>
2025-07-13 21:44:51 +0300

    +1 debug command

--- src/main/kotlin/cz/lukynka/minestom/gamejam/commands/DebugCommand.kt
+++ src/main/kotlin/cz/lukynka/minestom/gamejam/commands/DebugCommand.kt
@@ -4,7 +4,12 @@ import cz.lukynka.minestom.gamejam.entity.Zombie
 import cz.lukynka.minestom.gamejam.isAdmin
 import net.minestom.server.command.builder.Command
 import net.minestom.server.command.builder.arguments.ArgumentType
+import net.minestom.server.entity.Entity
+import net.minestom.server.entity.EntityType
 import net.minestom.server.entity.Player
+import net.minestom.server.entity.metadata.display.ItemDisplayMeta
+import net.minestom.server.item.ItemStack
+import net.minestom.server.item.Material
 
 object DebugCommand : Command("debug") {
     init {
@@ -21,5 +26,15 @@ object DebugCommand : Command("debug") {
             val zombie = Zombie()
             zombie.setInstance(world, pos)
         }, ArgumentType.Literal("spawn_zombie"))
+
+        addSyntax({ sender, context ->
+            if (sender !is Player) return@addSyntax
+
+            val en = Entity(EntityType.ITEM_DISPLAY)
+            en.setNoGravity(true)
+            val meta = en.entityMeta as ItemDisplayMeta
+            meta.itemStack = ItemStack.of(Material.values().random())
+            en.setInstance(sender.instance, sender.position.withView(0f, 0f))
+        }, ArgumentType.Literal("spawn_display_entity"))
     }
 }
