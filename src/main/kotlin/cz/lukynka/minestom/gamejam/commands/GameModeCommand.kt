d6463d7379498955e09b0dd6ae24f2d31967e635
p1k0chu <anatoliyid0207@gmail.com>
2025-07-14 19:38:36 +0300

    set gamemode cmd to lowercase

--- src/main/kotlin/cz/lukynka/minestom/gamejam/commands/GameModeCommand.kt
+++ src/main/kotlin/cz/lukynka/minestom/gamejam/commands/GameModeCommand.kt
@@ -1,6 +1,7 @@
 package cz.lukynka.minestom.gamejam.commands
 
 import net.minestom.server.command.builder.Command
+import net.minestom.server.command.builder.arguments.ArgumentEnum
 import net.minestom.server.command.builder.arguments.ArgumentType
 import net.minestom.server.entity.GameMode
 import net.minestom.server.entity.Player
@@ -12,6 +13,7 @@ object GameModeCommand : Command("gamemode") {
         }
 
         val arg = ArgumentType.Enum("gamemode", GameMode::class.java)
+            .setFormat(ArgumentEnum.Format.LOWER_CASED)
 
         addSyntax({ sender, context ->
             if (sender !is Player) return@addSyntax
