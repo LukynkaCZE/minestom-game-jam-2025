a58a3d2ab24ef6a9aef24e1f400f0fa2e06e9936
p1k0chu <anatoliyid0207@gmail.com>
2025-07-14 22:51:26 +0300

    fix elevator

--- src/main/kotlin/cz/lukynka/minestom/gamejam/game/Elevator.kt
+++ src/main/kotlin/cz/lukynka/minestom/gamejam/game/Elevator.kt
@@ -17,7 +17,6 @@ import cz.lukynka.minestom.gamejam.utils.shulkerMap
 import cz.lukynka.minestom.gamejam.utils.spawnItemDisplay
 import cz.lukynka.shulkerbox.minestom.conversion.toMinestomMap
 import net.kyori.adventure.bossbar.BossBar
-import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.textOfChildren
 import net.minestom.server.MinecraftServer
 import net.minestom.server.component.DataComponentMap
@@ -31,7 +30,6 @@ import net.minestom.server.item.ItemStack
 import net.minestom.server.item.Material
 import net.minestom.server.item.component.CustomModelData
 import net.minestom.server.timer.TaskSchedule
-import java.util.*
 import java.util.concurrent.CompletableFuture
 
 class Elevator(
@@ -147,12 +145,18 @@ class Elevator(
     }
 
     private fun tick() {
-        elevatorEntities.forEach {
+        val removed = elevatorEntities.removeIf {
             if (it.position.y >= elevatorPoofPlace) {
-                it.teleport(elevatorSpawn)
+                it.remove()
+                true
             } else {
                 it.teleport(it.position.add(speedPerTick))
+                false
             }
+        }
+
+        if (removed) {
+            addElevator()
         }
     }
 
@@ -163,6 +167,9 @@ class Elevator(
         ) {
             itemStack = elevatorItem
             scale = elevatorScale
+            transformationInterpolationDuration = 1
+            posRotInterpolationDuration = 1
+            transformationInterpolationStartDelta = -1
         }
 
         elevatorEntities.add(entity)
